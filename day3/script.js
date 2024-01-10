fe6fa0423eac3d6ec187d110c48681deb659a00d
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-01-10 17:43:40 +0530

    Day3 completed

--- day3/script.js
+++ day3/script.js
@@ -1,10 +1,18 @@
 const container = document.querySelector('.container');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link')
+const btn = document.querySelector('.btn-login')
+const btnclose = document.querySelector('.icon-close')
 
 registerLink.addEventListener('click', ()=>{
-    container.classList.add('.active')
+    container.classList.add('active')
 })
 loginLink.addEventListener('click', ()=>{
-    container.classList.remove('.active')
+    container.classList.remove('active')
+})
+btn.addEventListener('click', ()=>{
+    container.classList.add('active-btn')
+})
+btnclose.addEventListener('click', ()=>{
+    container.classList.remove('active-btn')
 })
